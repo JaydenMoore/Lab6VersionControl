74d0e2bc867a7659f3c278a91ffb0c2de582e961
JaydenMoore <jaydenmoore456@gmail.com>
2023-10-24 18:46:07 -0400

    Completed Exit Option

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ def main():
         original_password = input("Please enter your password to encode: ")
         encoded_password = encode(original_password)
         print("Your password has been encoded and stored!")
-    else:
+    elif option == 2:
         #Decodes the password and informs the user of the new password
         password = input("Please enter your password to decode: ")
         decoded_password = decode(password)
         print(f"The encoded password is {password}, and the original password is {decoded_password}.")
+    else:
+        exit()
 
 #Takes a password and adds 3 to each integer in the string.
 def encode(password):
@@ -30,6 +32,5 @@ def encode(password):
 
 def decode(password):
     pass
-    
 
 main()
